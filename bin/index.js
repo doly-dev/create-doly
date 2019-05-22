ae77e61c18e9cc346c74dbde34d5df0deaa437d7
roxy_huang <sixin_huang@outlook.com>
2019-05-22 22:49:00 +0800

    update version 0.0.7

--- bin/index.js
+++
@@ -1,5 +0,0 @@
-const run = require('../src/run');
-const list = require('../src/list');
-
-module.exports.run = run;
-module.exports.list = list;
---
+++ index.js
@@ -0,0 +1,5 @@
+const run = require('./src/run');
+const list = require('./src/list');
+
+module.exports.run = run;
+module.exports.list = list;
